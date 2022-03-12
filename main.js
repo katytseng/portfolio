b6f15605444f55f401a9fdeb9e89830b62829591
Katy Tseng <katy.tseng12@gmail.com>
2022-03-12 11:29:04 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function validateForm() {
+  if (document.myForm.name.value == "") {
+    alert("FirstName must be filled out");
+    document.myForm.name.focus();	 
+    return false;
+  }
+
+  if (document.myForm.email.value == "") {
+   alert("Email must be filled out");
+   document.myForm.email.focus();	 
+   return false;
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("homeButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
